e8b344cb47b8aaa93fc321ffa67554e19b7597b2
akless <andre.kless@web.de>
2018-06-28 21:57:51 +0200

    ccm v16.7.0: ccm.load supports loading of XML files

--- versions/ccm-16.7.0.js
+++ versions/ccm-16.7.0.js
@@ -2,7 +2,7 @@
  * @overview ccm framework
  * @author André Kless <andre.kless@web.de> 2014-2018
  * @license The MIT License (MIT)
- * @version latest (16.7.0)
+ * @version 16.7.0
  * @changes
  * version 16.7.0 (28.06.2018)
  * - ccm.load supports loading of XML files
