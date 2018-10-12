6a78fd14ee998f9d6a82bcbcf86dc29be41686cb
akless <andre.kless@web.de>
2018-10-12 22:06:22 +0200

    ccm v18.0.4: bug fix for ccm.helper.unescapeHTML

--- versions/ccm-18.0.4.js
+++ versions/ccm-18.0.4.js
@@ -2,7 +2,7 @@
  * @overview ccm framework
  * @author André Kless <andre.kless@web.de> 2014-2018
  * @license The MIT License (MIT)
- * @version latest (18.0.4)
+ * @version 18.0.4
  * @changes
  * version 18.0.4 (12.10.2018): bug fix for ccm.helper.unescapeHTML
  * version 18.0.3 (12.10.2018): bug fix for ccm.helper.format
