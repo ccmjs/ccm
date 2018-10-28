8e5831980ee8b57e49e82442b3b2614491cd377a
akless <andre.kless@web.de>
2018-10-29 00:00:25 +0100

    bug fix for ccm.helper.dataset

--- ccm.js
+++ ccm.js
@@ -1653,7 +1653,7 @@
         if ( !settings.key ) settings.key = self.helper.generateKey();
 
         // key is initial data? => take it as result
-        if ( self.helper.isDataset( settings.key ) ) return settings.key;
+        if ( self.helper.isDataset( settings.key ) ) return settings.convert ? settings.key : settings.convert( settings.key );
 
         /**
          * nearest user instance in ccm context tree
