227f1c65c446f1a58679b7715d8f17867635bb0f
akless <andre.kless@web.de>
2019-05-05 00:56:14 +0200

    ccm v20.1.2: bug fix for connected callback of ccm Custom Elements

--- versions/ccm-20.1.2.js
+++ versions/ccm-20.1.2.js
@@ -2,7 +2,7 @@
  * @overview ccm framework
  * @author André Kless <andre.kless@web.de> 2014-2019
  * @license The MIT License (MIT)
- * @version latest (20.1.2)
+ * @version 20.1.2
  * @changes
  * version 20.1.2 (05.05.2019):
  * - bug fix for connected callback of ccm Custom Elements
