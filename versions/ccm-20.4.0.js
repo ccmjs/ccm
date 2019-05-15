daf1d0d751586e951b32b862477ee5efe86a9852
akless <andre.kless@web.de>
2019-05-15 15:45:44 +0200

    ccm v20.4.0: allow '-' in dataset keys

--- versions/ccm-20.4.0.js
+++ versions/ccm-20.4.0.js
@@ -2,7 +2,7 @@
  * @overview ccm framework
  * @author André Kless <andre.kless@web.de> 2014-2019
  * @license The MIT License (MIT)
- * @version latest (20.4.0)
+ * @version 20.4.0
  * @changes
  * version 20.4.0 (15.05.2019):
  * - allow '-' in dataset keys
