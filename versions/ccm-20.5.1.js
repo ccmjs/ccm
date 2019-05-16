ca7b84518c3232439ba1d8f81404e6a0b724cdb9
akless <andre.kless@web.de>
2019-05-17 00:33:28 +0200

    ccm v20.5.1: bug fix for loading a framework version with subresource integrity

--- versions/ccm-20.5.1.js
+++ versions/ccm-20.5.1.js
@@ -2,7 +2,7 @@
  * @overview ccm framework
  * @author André Kless <andre.kless@web.de> 2014-2019
  * @license The MIT License (MIT)
- * @version latest (20.5.1)
+ * @version 20.5.1
  * @changes
  * version 20.5.1 (17.05.2019):
  * - bug fix for loading a framework version with subresource integrity
