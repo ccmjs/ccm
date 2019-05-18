afa3e5e51859a0470841ecbd471bd65a2cd1a281
akless <andre.kless@web.de>
2019-05-19 01:31:39 +0200

    ccm v20.6.2: bug fix change of component used framework version via config

--- versions/ccm-20.6.2.js
+++ versions/ccm-20.6.2.js
@@ -2,7 +2,7 @@
  * @overview ccm framework
  * @author André Kless <andre.kless@web.de> 2014-2019
  * @license The MIT License (MIT)
- * @version latest (20.6.2)
+ * @version 20.6.2
  * @changes
  * version 20.6.2 (19.05.2019):
  * - bug fix change of component used framework version via config
