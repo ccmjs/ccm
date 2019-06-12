780a233c04c6948d88117f34d88d27d56522f46c
akless <andre.kless@web.de>
2019-06-13 00:35:56 +0200

    ccm v21.0.1: try-catch for JSON.parse in ccm.load at data exchange result

--- versions/ccm-21.0.1.js
+++ versions/ccm-21.0.1.js
@@ -2,7 +2,7 @@
  * @overview ccm framework
  * @author André Kless <andre.kless@web.de> 2014-2019
  * @license The MIT License (MIT)
- * @version latest (21.0.1)
+ * @version 21.0.1
  * @changes
  * version 21.0.1 (12.06.2019):
  * - try-catch for JSON.parse in ccm.load at data exchange result
