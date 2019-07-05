4148449fa09e1bb9f774b6587858710cbba16039
akless <andre.kless@web.de>
2019-07-05 14:40:01 +0200

    ccm v21.1.2: JSON.parse only for strings in ccm.load

--- versions/ccm-21.1.2.js
+++ versions/ccm-21.1.2.js
@@ -2,7 +2,7 @@
  * @overview ccm framework
  * @author André Kless <andre.kless@web.de> 2014-2019
  * @license The MIT License (MIT)
- * @version latest (21.1.2)
+ * @version 21.1.2
  * @changes
  * version 21.1.2 (05.07.2019):
  * - JSON.parse only for strings in ccm.load
