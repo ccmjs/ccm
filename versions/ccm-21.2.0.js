9644656b0b4c006d1a28bbe2010a5e4836cc7a95
akless <andre.kless@web.de>
2019-07-12 20:42:23 +0200

    ccm v21.2.0: ccm.load allows setup of request headers

--- versions/ccm-21.2.0.js
+++ versions/ccm-21.2.0.js
@@ -2,7 +2,7 @@
  * @overview ccm framework
  * @author André Kless <andre.kless@web.de> 2014-2019
  * @license The MIT License (MIT)
- * @version latest (21.2.0)
+ * @version 21.2.0
  * @changes
  * version 21.2.0 (15.07.2019):
  * - ccm.load allows setup of request headers
