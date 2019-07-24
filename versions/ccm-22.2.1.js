65aafed89feba4383e433b1a5289ae41d6e66609
akless <andre.kless@web.de>
2019-07-24 11:19:15 +0200

    ccm v22.2.1: bug fix for ccm.helper.html2json

--- versions/ccm-22.2.1.js
+++ versions/ccm-22.2.1.js
@@ -2,7 +2,7 @@
  * @overview ccm framework
  * @author André Kless <andre.kless@web.de> 2014-2019
  * @license The MIT License (MIT)
- * @version latest (22.2.1)
+ * @version 22.2.1
  * @changes
  * version 22.2.1 (24.07.2019):
  * - bug fix for ccm.helper.html2json
