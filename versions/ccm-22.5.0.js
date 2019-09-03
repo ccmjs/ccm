9d1d04933b6d538dbfe144111227a56e6c3ec230
akless <andre.kless@web.de>
2019-09-03 14:00:51 +0200

    ccm v22.5.0 - ccm.helper.generateConfig accepts many forms of HTML - ccm.helper.html allows advanced settings like 'no_evaluation' (no evaluation of ccm HTML Elements) - ccm HTML Elements contained in Light DOM are not evaluated

--- versions/ccm-22.5.0.js
+++ versions/ccm-22.5.0.js
@@ -2,7 +2,7 @@
  * @overview ccm framework
  * @author André Kless <andre.kless@web.de> 2014-2019
  * @license The MIT License (MIT)
- * @version latest (22.5.0)
+ * @version 22.5.0
  * @changes
  * version 22.5.0 (03.09.2019):
  * - ccm.helper.generateConfig accepts many forms of HTML
