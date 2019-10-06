16af155f1150e0446b212e7da5760f0f27cb6f80
akless <andre.kless@web.de>
2019-10-06 10:58:48 +0200

    ccm v23.0.0 - set Shadow DOM mode via config: 'open', 'closed' or 'none' - default Shadow DOM mode is 'closed' - set instance root via HTML ID

--- versions/ccm-23.0.0.js
+++ versions/ccm-23.0.0.js
@@ -2,7 +2,7 @@
  * @overview ccm framework
  * @author André Kless <andre.kless@web.de> 2014-2019
  * @license The MIT License (MIT)
- * @version latest (23.0.0)
+ * @version 23.0.0
  * @changes
  * version 23.0.0 (06.10.2019):
  * - set Shadow DOM mode via config: 'open', 'closed' or 'none'
