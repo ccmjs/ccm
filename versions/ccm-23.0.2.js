8a30081b1f177fc6123e4d877a1abac0e89fe129
akless <andre.kless@web.de>
2019-10-08 10:25:47 +0200

    ccm v23.0.2: bug fix for set initial values of a multiple selector box

--- versions/ccm-23.0.2.js
+++ versions/ccm-23.0.2.js
@@ -2,7 +2,7 @@
  * @overview ccm framework
  * @author André Kless <andre.kless@web.de> 2014-2019
  * @license The MIT License (MIT)
- * @version latest (23.0.2)
+ * @version 23.0.2
  * @changes
  * version 23.0.2 (08.10.2019):
  * - bug fix for set initial values of a multiple selector box
