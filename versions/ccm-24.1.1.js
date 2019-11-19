8810e73942308c9ee90c08a5f4dc7e01e528405f
akless <andre.kless@web.de>
2019-11-19 10:27:58 +0100

    ccm v24.1.1: bug fix for ccm.helper.toDotNotation: no manipulation of original passed data

--- versions/ccm-24.1.1.js
+++ versions/ccm-24.1.1.js
@@ -2,7 +2,7 @@
  * @overview ccm framework
  * @author André Kless <andre.kless@web.de> 2014-2019
  * @license The MIT License (MIT)
- * @version latest (24.1.1)
+ * @version 24.1.1
  * @changes
  * version 24.1.1 (19.11.2019):
  * - bug fix for ccm.helper.toDotNotation: no manipulation of original passed data
