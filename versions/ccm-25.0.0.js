71353016a60236c51f981c5cc118faff7eea1c05
akless <andre.kless@web.de>
2020-02-25 22:33:46 +0100

    bug fix

--- versions/ccm-25.0.0.js
+++ versions/ccm-25.0.0.js
@@ -399,7 +399,7 @@
     function useWebsocket( params ) { return new Promise( resolve => {
 
       const key = self.helper.generateKey();
-      callbacks[ key ] = result => Number.isInteger( result ) ? result => checkError( result, reject ) : resolve;
+      callbacks[ key ] = result => Number.isInteger( result ) ? checkError( result, reject ) : resolve( result );
       params.callback = key;
       that.socket.send( self.helper.stringify( params ) );
 
