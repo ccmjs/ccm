ab2f4a014d528ab972219d27f91f251968f2fdf1
akless <andre.kless@web.de>
2020-03-03 20:47:32 +0100

    ccm v25.1.0: added count operation for datastores

--- versions/ccm-25.1.0.js
+++ versions/ccm-25.1.0.js
@@ -2,7 +2,7 @@
  * @overview ccm framework
  * @author André Kless <andre.kless@web.de> 2014-2020
  * @license The MIT License (MIT)
- * @version latest (25.1.0)
+ * @version 25.1.0
  * @changes
  * version 25.1.0 (20.03.2020)
  * - added count operation for datastores
