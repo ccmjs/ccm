45129f99dd2f1cfc0d6a60497388d597894ad610
akless <andre.kless@web.de>
2020-03-27 22:12:29 +0100

    ccm v25.2.1: bug fix for loading HTML templates via <ccm-template>

--- versions/ccm-25.2.1.js
+++ versions/ccm-25.2.1.js
@@ -2,7 +2,7 @@
  * @overview ccm framework
  * @author André Kless <andre.kless@web.de> 2014-2020
  * @license The MIT License (MIT)
- * @version latest (25.2.1)
+ * @version 25.2.1
  * @changes
  * version 25.2.1 (27.03.2020)
  * - bug fix for loading HTML templates via <ccm-template>
