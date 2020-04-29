4abab5d75d0b7f0fe59e8e955f4a86c41f8b7a15
akless <andre.kless@web.de>
2020-04-29 21:21:02 +0200

    bug fix

--- versions/ccm-25.5.2.js
+++ versions/ccm-25.5.2.js
@@ -2,7 +2,7 @@
  * @overview ccm framework
  * @author André Kless <andre.kless@web.de> 2014-2020
  * @license The MIT License (MIT)
- * @version latest (25.5.2)
+ * @version 25.5.2
  * @changes
  * version 25.5.2 (29.04.2020)
  * - bug fix for ccm.load when loading big data
