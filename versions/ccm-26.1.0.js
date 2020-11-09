4880ca12e8cdaabad88c203d3a7ef37179e40fd0
akless <andre.kless@web.de>
2020-11-09 10:42:24 +0100

    ccmjs v26.1.0: user token and realm can directly be passed via datastore accessor configuration

--- versions/ccm-26.1.0.js
+++ versions/ccm-26.1.0.js
@@ -5,7 +5,7 @@
  * and defines the Custom Element <code>\<ccm-app\></code>.
  * @author André Kless <andre.kless@web.de> 2014-2020
  * @license The MIT License (MIT)
- * @version latest (26.1.0)
+ * @version 26.1.0
  * @changes
  * version 26.1.0 (09.11.2020)
  * - user token and realm can directly be passed via datastore accessor configuration
