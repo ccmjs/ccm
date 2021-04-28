8d7434c1181761e360d635aa135f18af608dcac2
akless <andre.kless@web.de>
2021-04-28 10:19:05 +0200

    ccmjs v26.4.0: focusable website area (allows component-specific keyboard events)

--- versions/ccm-26.4.0.js
+++ versions/ccm-26.4.0.js
@@ -5,7 +5,7 @@
  * and defines the Custom Element <code>\<ccm-app\></code>.
  * @author André Kless <andre.kless@web.de> 2014-2021
  * @license The MIT License (MIT)
- * @version latest (26.4.0)
+ * @version 26.4.0
  * @changes
  * version 26.4.0 (28.04.2021)
  * - focusable website area (allows component-specific keyboard events)
