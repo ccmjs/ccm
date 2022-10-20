219edc3e52bb35557145eb8d557ef07a2bfe6a31
akless <andre.kless@web.de>
2022-10-20 14:07:33 +0200

    ccmjs v27.4.1: bugfix in ccm.helper.html for checked checkboxes

--- versions/ccm-27.4.1.js
+++ versions/ccm-27.4.1.js
@@ -5,7 +5,7 @@
  * and defines the Custom Element <code>\<ccm-app\></code>.
  * @author André Kless <andre.kless@web.de> 2014-2022
  * @license The MIT License (MIT)
- * @version latest (27.4.1)
+ * @version 27.4.1
  * @changes
  * version 27.4.1 (20.10.2022)
  * - bugfix in ccm.helper.html for checked checkboxes
